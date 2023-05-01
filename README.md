5d01c07306d1c7ee92b301ecf6e313ee48f93835
Danny McCabe <94184743+dannymccabe@users.noreply.github.com>
2023-05-01 13:01:14 -0300

    updating site banner sizes

--- README.md
+++
@@ -1,1 +0,0 @@
-# Landin-website2
---
+++ _next/static/uqgLkMwWr44CNAjAyze6p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/814-3cf97f3d7f95146a.js","static/chunks/pages/index-7232a1e9e4290328.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
